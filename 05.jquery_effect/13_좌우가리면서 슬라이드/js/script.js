6a8acb8faac58d6c946329b800830349a2c6d9f9
seongeun040624 <seongeun040624@gmail.com>
2026-01-14 11:20:54 +0900

    0114

--- 05.jquery_effect/13_좌우가리면서 슬라이드/js/script.js
+++ 05.jquery_effect/13_좌우가리면서 슬라이드/js/script.js
@@ -23,5 +23,70 @@ $(function(){
         page.eq(next).stop().animate({width: '100%'}, 1500);
 
         current++;
+
+        pagenation.removeClass('on');
+        pagenation.eq(next).addClass('on');
     }
-})
+
+    $('.page, .btn>a, .pagenation>a').hover(function(){
+        clearInterval(timer)
+    }, function(){
+        timer = setInterval(slideWidth, 2000)
+    });
+
+    nextBtn.click(slideWidth);
+    prevBtn.click(function(){
+        if(current < 0) current=2;
+
+        let prev = current -1;
+        if(current < 0) prev=3;
+
+        page.eq(current).css({zIndex: 0, width: '100%'}).addClass('right');
+        page.eq(current).children('img').addClass('right');
+        page.eq(current).stop().animate({width: '0%'}, 1500);
+
+        page.eq(prev).css({zIndex: 1, width: 0}).removeClass('right');
+        page.eq(prev).children('img').removeClass('right');
+        page.eq(prev).stop().animate({width: '100%'}, 1500);
+
+        current--;
+
+        pagenation.removeClass('on');
+        pagenation.eq(prev).addClass('on');
+    });
+
+    pagenation.click(function(){
+        let tg = $(this).index();
+        
+        if(current > tg){
+            page.eq(current).css('zIndex', 0).removeClass('right');
+            page.eq(current).children('img').removeClass('right');
+            page.eq(current).stop().animate({width: '0%'}, 1500);
+
+            page.eq(tg).css('zIndex', 1).addClass('right');
+            page.eq(tg).children('img').addClass('right');
+            page.eq(tg).stop().animate({width: '100%'}, 1500);
+
+            current = tg;
+
+            pagenation.removeClass('on');
+            pagenation.eq(tg).addClass('on');
+        }else if(current == tg){
+            return false; //아무것도 하지 않게
+        }else{
+            page.eq(current).css({zIndex: 0, width: '100%'}).addClass('right');
+            page.eq(current).children('img').addClass('right');
+            page.eq(current).stop().animate({width: '0%'}, 1500);
+
+            page.eq(tg).css({zIndex: 1, width: 0}).removeClass('right');
+            page.eq(tg).children('img').removeClass('right');
+            page.eq(tg).stop().animate({width: '100%'}, 1500);
+
+            current = tg;
+
+            pagenation.removeClass('on');
+            pagenation.eq(tg).addClass('on');
+        }
+
+    })
+});
---
+++ 05.jquery_effect/swiper/js/script.js
@@ -0,0 +1,29 @@
+var swiper = new Swiper(".mySwiper1", {
+      slidesPerView: 1,
+      spaceBetween: 0,
+      loop: true,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    });
+
+
+var swiper2 = new Swiper(".mySwiper2", {
+      pagination: {
+        el: ".swiper-pagination",
+        type: "fraction",
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    });
